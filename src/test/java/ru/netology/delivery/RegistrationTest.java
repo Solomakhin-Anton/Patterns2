be82eb80798a99cff3dacde131e4bd315e7d7a60
Solomakhin Anton <s.anton45@yandex.ru>
2020-05-12 13:19:56 +0300

    Notes fixed

--- src/test/java/ru/netology/delivery/RegistrationTest.java
+++ src/test/java/ru/netology/delivery/RegistrationTest.java
@@ -1,13 +1,11 @@
 package ru.netology.delivery;
 
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.delivery.UserGenerator.*;
-
 
 public class RegistrationTest {
     UserGenerator userGenerator = new UserGenerator();
@@ -19,8 +17,8 @@ public class RegistrationTest {
         $("[name=login]").setValue(validValidActiveUser.getLogin());
         $("[name=password]").setValue(validValidActiveUser.getPassword());
         $(".button__text").click();
-        $(".notification_status_error").shouldBe(hidden);
-        SelenideElement response = $(".body");
+        $(".App_appContainer__3jRx1 h2.heading").waitUntil(exist,5000);
+        $(".App_appContainer__3jRx1 h2.heading").shouldHave(matchesText("Личный кабинет"));
     }
 
     @Test
@@ -30,8 +28,8 @@ public class RegistrationTest {
         $("[name=login]").setValue(validValidAcBlockedUser.getLogin());
         $("[name=password]").setValue(validValidAcBlockedUser.getPassword());
         $(".button__text").click();
-        $(".notification_status_error").shouldBe(visible);
-        SelenideElement response = $(withText("Ошибка"));
+        $(".notification_status_error").waitUntil(visible,5000);
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Пользователь заблокирован"));
     }
 
     @Test
@@ -41,7 +39,8 @@ public class RegistrationTest {
         $("[name=login]").setValue(userWithIncorrectPassword.getLogin());
         $("[name=password]").setValue(userWithIncorrectPassword.getPassword());
         $(".button__text").click();
-        $(".notification_status_error").shouldBe(visible);
+        $(".notification_status_error").waitUntil(visible,5000);
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
     }
 
     @Test
@@ -51,7 +50,8 @@ public class RegistrationTest {
         $("[name=login]").setValue(userWithIncorrectLogin.getLogin());
         $("[name=password]").setValue(userWithIncorrectLogin.getPassword());
         $(".button__text").click();
-        $(".notification_status_error").shouldBe(visible);
+        $(".notification_status_error").waitUntil(visible,5000);
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
     }
 
     @Test
@@ -61,7 +61,7 @@ public class RegistrationTest {
         $("[name=login]").setValue(userWithIncorrectLoginAndPassword.getLogin());
         $("[name=password]").setValue(userWithIncorrectLoginAndPassword.getPassword());
         $(".button__text").click();
-        $(".notification_status_error").shouldBe(visible);
+        $(".notification_status_error").waitUntil(visible,5000);
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
     }
-
 }
